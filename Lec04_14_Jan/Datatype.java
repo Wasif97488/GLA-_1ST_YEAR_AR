46c9e112bc83352ed699c9c1e4b60901d27be633
Wasif97488 <w4wasifrock@gmail.com>
2026-01-17 07:39:54 +0530

    Datatype and Operator

---
+++ Lec04_14_Jan/Datatype.java
@@ -0,0 +1,32 @@
+class Datatype
+{
+public static void main(String args[])
+{
+byte b = 10;
+byte c = 127;
+//byte d = 128;
+byte e = -128;
+
+
+//short datatype
+
+short s = 32767;
+//short s1 = 32768;
+short s2 = -32768;
+System.out.println(s2);
+
+//int datatype
+
+int i =3774743;
+System.out.println(i);
+
+//long datatype 
+long l = 23232323;
+long l1 = 9748862437l;
+long l2 = 36637363829827l;
+System.out.println(l2);
+
+
+
+}
+}
---
+++ Lec05_15_Jan/Datatype4.java
@@ -0,0 +1,31 @@
+class Datatype4
+{
+public static void main(String args[])
+{
+short s = 24253;
+short s1 = 32767;
+//short s2 = 32768;
+
+int i =77474743;
+//int i1 = 4746746111;
+System.out.println(i);
+
+
+// long datatype
+
+long l = 43527298;
+System.out.println(l);
+
+long l1 = 4637387620l;
+System.out.println(l1);
+
+Byte b = 63;
+Short r =3737;
+
+//Int id = 37737;
+
+Long l3 = 373773;
+
+
+}
+}
