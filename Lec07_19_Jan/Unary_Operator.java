8b775f4f9467c903b527c45fff8285b99f8acf4b
Wasif97488 <w4wasifrock@gmail.com>
2026-01-20 14:23:23 +0530

    Arithmetic and String Concatenation

---
+++ Lec07_19_Jan/Unary_Operator.java
@@ -0,0 +1,51 @@
+class Unary_Operator
+{
+public static void main(String args[])
+{
+int a=5;
+int b = ++a;
+System.out.println(a);
+System.out.println(b);
+
+
+int x=5;
+int y = --x;
+System.out.println(x);
+System.out.println(y);
+
+//int i=4;
+//int j = ++(++i);
+//System.out.println(j);
+
+//final int m = 4;
+//m++;
+//System.out.println(m);
+
+//byte o = 10;
+//o++;
+//System.out.println(o);
+
+byte o = 10;
+//o=o+1;
+//int n=o+1;
+o=(byte)(o+1);
+
+System.out.println(o);
+
+
+byte e = 1;
+byte f = 2;
+byte g = e+f;
+System.out.println(g);
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ Lec08_20_Jan/Operator.java
@@ -0,0 +1,20 @@
+class Operator
+{
+public static void main(String args[])
+{
+int a=1,b=2,c=3;
+String s="wasif";
+System.out.println(a+b+c+s);
+System.out.println(s+a+b+c);
+System.out.println(a+s+b+c);
+
+
+s=a+b+c;
+System.out.println(s);
+
+
+
+
+
+}
+}
